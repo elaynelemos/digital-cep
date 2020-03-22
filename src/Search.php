3ef42faa2294ec7c2ea80bc60c33e408dc454ac3
elaynelemos <elayne.l.lemos@gmail.com>
2020-03-22 11:59:01 -0300

    Adding other zipcode web services

--- src/Search.php
+++ src/Search.php
@@ -3,11 +3,51 @@
 namespace Lay\DigitalCep;
 
 class Search {
-  private $url = "https://viacep.com.br/ws/";
+  private $url = [];
 
   public function getAddressFromZipcode(string $zipCode): array {
+    $this->url[] = "https://viacep.com.br/ws/";
+    $this->url[] = "http://cep.la/";
+    $this->url[] = "https://apps.widenet.com.br/busca-cep/api/cep/";
+    $url = $this->url;
+    $opts = [
+      "http" => [
+          "method" => "GET",
+          "header" => "Accept: application/json\r\n",
+          'ignore_errors' => true
+      ]
+    ];
+    $context = stream_context_create($opts);
     $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
-    $get = file_get_contents($this->url . $zipCode . "/json");
-    return (array) json_decode($get);
+    $get = NULL;
+    foreach ($url as $str) {
+      try {
+        switch ($str) {
+          case $url[0]:
+            $get = @file_get_contents($str . $zipCode . "/json", false, $context);
+            break;
+          case $url[1]:
+            $get = @file_get_contents($str . $zipCode, false, $context);
+            break;
+          case $url[2]:
+            $get = @file_get_contents($str . $zipCode . ".json", false, $context);
+            $target = array("code", "state", "city", "district", "address");
+            $src = array("cep", "uf", "cidade", "bairro", "logradouro");
+            $len = count($target);
+            for ($i = 0; $i < $len; $i++) {
+              $get = str_replace($target[$i], $src[$i], $get);
+            }
+            break;
+        }
+      }
+      catch(Exception $e) {
+        echo 'Error: '. $str . 'not available!' . PHP_EOL;
+      }
+      finally {
+        $get = json_decode($get);
+      }
+      if(!empty($get)) break;
+    }
+    return (array) $get;
   }
 }
