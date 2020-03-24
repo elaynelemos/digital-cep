f7914dae19cfc7acf2c65d49b9e8d2705499263c
elaynelemos <elayne.l.lemos@gmail.com>
2020-03-24 11:12:30 -0300

    Unifying answers from the APIs

--- src/Search.php
+++ src/Search.php
@@ -5,11 +5,8 @@ namespace Lay\DigitalCep;
 class Search {
   private $url = [];
 
-  public function getAddressFromZipcode(string $zipCode): array {
-    $this->url[] = "https://viacep.com.br/ws/";
-    $this->url[] = "http://cep.la/";
-    $this->url[] = "https://apps.widenet.com.br/busca-cep/api/cep/";
-    $url = $this->url;
+  private function getContent(string $url, string $zipCode, 
+  string $complement): string {
     $opts = [
       "http" => [
           "method" => "GET",
@@ -19,24 +16,73 @@ class Search {
     ];
     $context = stream_context_create($opts);
     $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+    $get = @file_get_contents($url . $zipCode . $complement, false, $context);
+    
+    return $get;
+  }
+  private function replaceKeys(string $source, array $oldKeys, 
+  array $newKeys): ?string {
+    $len = count($oldKeys);
+    if ($len === count($newKeys)) {
+      for ($i = 0; $i < $len; $i++) {
+        $source = str_replace($oldKeys[$i], $newKeys[$i], $source);
+      }
+      return $source;
+    } else {
+      return null;
+    }
+  }
+  
+  private function eliminateKeys(array $source, array $keys): array {
+    $i = null;
+    $keysLength = count($keys);
+    for ($i = 0; $i < $keysLength; $i++) {
+      unset($source[$keys[$i]]);
+    }
+    return $source;
+  } 
+  
+  private function joinKeys(array $source, array $destKeys, 
+  array $oldKeys): array {
+    $keysLength = count($destKeys);
+    $i = null;
+    for ($i = 0; $i < $keysLength; $i++) {
+      if(@$source[$destKeys[$i]] != null && @$source[$oldKeys[$i]] != null) {
+        $source[$destKeys[$i]] = $source[$destKeys[$i]] . ' - ' .
+        $source[$oldKeys[$i]];
+        unset($source[$oldKeys[$i]]);
+      }
+    }
+    @$source["cep"] = str_replace( "-", "", $source["cep"]);
+    return $source;
+  }
+  
+  public function getAddressFromZipcode(string $zipCode): array {
+    $this->url[] = "https://viacep.com.br/ws/";
+    $this->url[] = "http://cep.la/";
+    $this->url[] = "https://apps.widenet.com.br/busca-cep/api/cep/";
+    $url = $this->url;
     $get = NULL;
+    if(strlen(preg_replace("/[^0-9]/", "", $zipCode)) !== 8 || 
+    (int)$zipCode === 0) {
+      return array();
+    }
     foreach ($url as $str) {
       try {
         switch ($str) {
           case $url[0]:
-            $get = @file_get_contents($str . $zipCode . "/json", false, $context);
+            $get = $this->getContent($str, $zipCode, "/json");
+            $get = $this->replaceKeys($get, array("localidade"), 
+              array("cidade"));
             break;
           case $url[1]:
-            $get = @file_get_contents($str . $zipCode, false, $context);
+            $get = $this->getContent($str, $zipCode, "");
             break;
           case $url[2]:
-            $get = @file_get_contents($str . $zipCode . ".json", false, $context);
-            $target = array("code", "state", "city", "district", "address");
-            $src = array("cep", "uf", "cidade", "bairro", "logradouro");
-            $len = count($target);
-            for ($i = 0; $i < $len; $i++) {
-              $get = str_replace($target[$i], $src[$i], $get);
-            }
+            $get = $this->getContent($str, $zipCode, ".json");
+            $get = $this->replaceKeys($get, 
+              array("code", "state", "city", "district", "address"), 
+              array("cep", "uf", "cidade", "bairro", "logradouro"));
             break;
         }
       }
@@ -48,6 +94,10 @@ class Search {
       }
       if(!empty($get)) break;
     }
-    return (array) $get;
+    $get = $this->joinKeys((array)$get, array("logradouro"), 
+      array("complemento"));
+    $get = $this->eliminateKeys($get, array("unidade", "ibge", "status", 
+      "gia", "ok", "statusText", "complemento"));
+    return $get;
   }
 }
